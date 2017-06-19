812c989a769b1d16dbc05ae6a7b1f40373596ba8
avinashg8085 <avinashgupta8085@gmail.com>
2017-06-19 21:26:50 +0530

    Create add tow integer value.c

---
+++ add tow integer value.c
@@ -0,0 +1,12 @@
+//add to tow integer
+//header file
+#include<stdio.h>
+//main
+main()
+//input tow integer value
+{
+    //Two integers entered by user 
+    int a=7,b=4;
+    printf("%d+%d=%d",a,b,a+b);
+}
+output: 7+4=11
