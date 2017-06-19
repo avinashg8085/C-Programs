7c83da4d3d79eaf5d8d876a2345e894d5f4335e3
avinashg8085 <avinashgupta8085@gmail.com>
2017-06-19 16:22:34 +0530

    Create 3.c

---
+++ 3.c
@@ -0,0 +1,19 @@
+// To find Quotient and remainder
+// header file
+#include<stdio.h>
+// main calling function
+main()
+{
+    // variables
+    int a, b;
+    // Initialize values
+    a = 25;
+    b = 4;
+    // Calculation
+    // OUTPUT
+    // Quotient
+    printf("%d / %d = %d\n", a, b, a/b);
+    // Remainder
+    printf("%d %% %d = %d\n", a, b, a%b);
+}
+
