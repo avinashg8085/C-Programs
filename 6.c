4f60748b498f270f2907f44e7a0aeba2522c5347
avinashg8085 <avinashgupta8085@gmail.com>
2017-06-19 15:49:03 +0530

    Adding File to get ascii value of character

---
+++ 6.c
@@ -0,0 +1,13 @@
+// To find the ASCII value of character
+// header files
+#include<stdio.h>
+// Main Calling Function
+int main(){
+    // Variables
+    char ch;
+    // Input a Character from User
+    scanf("%c", &ch);
+    // Output its ASCII Value
+    printf("%d", ch);
+return 0;
+}
