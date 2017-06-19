c053b9892cd617724a5135127e9157f773158bfe
avinashg8085 <avinashgupta8085@gmail.com>
2017-06-19 21:47:35 +0530

    Create Multiply two Floating Point Numbers.c

---
+++ Multiply two Floating Point Numbers.c
@@ -0,0 +1,13 @@
+//find to Multiply two Floating Point Numbers
+//header file
+#include<stdio.h>
+//main
+main()
+//put the tow floating point number
+{
+    double a=2.4,b=5.8;
+    //printf()show the value a and b
+    printf("%1f+%1f=%1f",a,b,a+b);
+}
+
+//output:2.4+5.8=8.200000
