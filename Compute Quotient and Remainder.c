a16ffd867ccee643cdd54a902af9da29b76f2ce7
avinashg8085 <avinashgupta8085@gmail.com>
2017-06-20 07:37:36 +0530

    Create Compute Quotient and Remainder.c

---
+++ Compute Quotient and Remainder.c
@@ -0,0 +1,17 @@
+//find to Quotient and Remainder
+//header file
+#include<stdio.h>
+//main calling function
+main()
+{
+    int a,b;
+    //input
+    scanf("%d %d",&a,&b);
+    //output
+    //quotient
+    printf("%d / %d=%d\n",a,b,a/b);
+    //remainder
+    printf("%d %% %d= %d\n",a,b,a%b);
+}
+//output:25/4=6
+         25%4=1
