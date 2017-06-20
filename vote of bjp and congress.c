f270d7ef2cf690d8e5f4c5ec937893d80fe57fb7
avinashg8085 <avinashgupta8085@gmail.com>
2017-06-20 09:54:10 +0530

    Create vote of bjp and congress.c

---
+++ vote of bjp and congress.c
@@ -0,0 +1,18 @@
+//vote of (bjp and congress)
+//header file
+#include<stdio.h>
+//main calling function
+main()
+{
+    int number;
+    printf("fever of candidate:");
+    scanf("%d",&number);
+    if (number %2 == 0)
+       printf("%d is a bjp.",number);
+    else
+        printf("%d is a congress",number);
+}
+
+
+
+
