c55f979aa35068afd4ff27df98b53a41802c060e
avinashg8085 <avinashgupta8085@gmail.com>
2017-06-20 08:14:47 +0530

    Create Program to Check Even or Odd.c

---
+++ Program to Check Even or Odd.c
@@ -0,0 +1,18 @@
+//find the Even or Odd
+//header file
+#include<stdio.h>
+//main calling function
+main()
+{
+    int number;
+    printf("enter the integer:");
+    scanf("%d",&number);
+    if (number %2 == 0)
+       printf("%d is a  even.",number);
+    else
+        printf("%d is a odd",number);
+
+
+}
+output: -7
+-7 is a odd number
