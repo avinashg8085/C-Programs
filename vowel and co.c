685b4f5e0cf3563f5863de4e2a02a780349fc8ee
avinash <avinashgupta8085@gmail.com>
2017-06-22 16:31:52 +0530

    Update vowel and co.c

--- vowel and co.c
+++ vowel and co.c
@@ -32,3 +32,31 @@ main()
         printf("Not a Character;");
     }
 }
+
+
+
+
+
+#include<stdio.h>
+main()
+{
+    char ch;
+    printf("inter the character:");
+    scanf("%c",&ch);
+    if ((ch >='a' && ch<='z')||(ch>='A'&& ch<='Z')){
+     if ((ch =='a'|| ch=='e'||ch=='i'||ch=='o'||ch=='u') ||(ch =='A'|| ch=='E'||ch=='I'||ch=='O'||ch=='U')){
+        printf("it is a vowel");
+    }else{
+
+
+ printf("it is a Consonant");
+}
+
+
+} else{
+        printf(" not a character:");
+    }
+
+}
+
+
