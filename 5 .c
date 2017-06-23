74a826f642691b97cf994bb80c3a3d3c28ce849c
avinash <avinashgupta8085@gmail.com>
2017-06-23 17:00:16 +0530

    Create 5 .c

---
+++ 5 .c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+main()
+{
+    int a;
+    printf("inter the any number:");
+    scanf("%d",&a);
+    if ( a%5 == 0)
+        printf("this is a divided of 5");
+    else
+        printf(" not a divided of 5");
+}
