712377fc72d039113b69e0b022f762235820ff72
avinash <avinashgupta8085@gmail.com>
2017-06-23 16:59:05 +0530

    Create  a group and b group.c

---
+++ a group and b group.c
@@ -0,0 +1,14 @@
+// find the a group and b group
+//header file
+#include<stdio.h>
+// main calling function
+main()
+{
+    int a;
+    printf("inter the  any number:");
+    scanf("%d",&a);
+    if (a > 0 && a<= 10)
+        printf(" this is A group");
+    else
+        printf("this is B group");
+}
