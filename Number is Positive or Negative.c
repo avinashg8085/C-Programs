6abc5bb6d411ed9384c58711d2107943a1bd3c73
avinash <avinashgupta8085@gmail.com>
2017-06-23 16:58:24 +0530

    Create Number is Positive or Negative.c

---
+++ Number is Positive or Negative.c
@@ -0,0 +1,16 @@
+//C Program to Check Whether a Number is Positive or Negative
+// header file
+#include<stdio.h>
+//main calling function
+main()
+{
+    int a;
+    printf("inter the number:");
+    scanf("%d",&a);
+    if (a >= 0)
+        printf("this is tha positive number");
+    else
+        printf("this is a negative number");
+}
+
+
