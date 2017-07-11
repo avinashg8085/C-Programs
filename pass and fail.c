685ab979803eede38fc0d70cfd0eca39d80a8c98
avinash <avinashgupta8085@gmail.com>
2017-07-11 13:44:23 +0530

    Create pass and fail.c

---
+++ pass and fail.c
@@ -0,0 +1,14 @@
+// find pass and fail
+#include<stdio.h>
+//header file
+// main calling function
+main()
+{
+    int number;
+    printf("inter the number:");
+    scanf("%d",&number);
+    if (number >= 50)
+        printf("this is the pass");
+    else
+        printf("this is the fail");
+}
